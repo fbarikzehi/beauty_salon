667e5a7a7d8ef79c79aa1b65444393024b517a71
Fereidoon Barikzehi <f.barikzehi.dev@gmail.com>
2025-08-03 18:49:01 +0330

    Delete Beauty.Mobile/personnel directory

--- Beauty.Mobile/personnel/constants/EndPointUrls.js
+++
@@ -1,7 +0,0 @@
-export default {
-  baseApiUrl: '',
-  baseSiteUrl: '',
-  login: '/account/login',
-  appointmentGetAllNotDoneByDate: '/appointment/getAllNotDoneByDate?',
-  appointmentDoneService: '/Appointment/DoneService',
-};
